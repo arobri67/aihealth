a847fbc163457c51379627e9fbc79b186785d473
Arnaud <arnaud.obri@proton.me>
2024-11-20 14:28:32 +0100

    add metadata for companies and categories pages

--- src/app/categories/page.tsx
+++ src/app/categories/page.tsx
@@ -1,6 +1,32 @@
+import { Metadata } from "next";
+
 import CategoryFilter from "@/components/category-filter";
 import SectionHeader from "@/components/section-header";
+import { env } from "@/env/client";
 import { getCategories } from "@/lib/actions";
+import { generateCommonMetadata } from "@/lib/metadata";
+
+const title = "All Categories of AI for Healthcare Hub";
+const description =
+  "Currently we only have 10 categories, but we will add more soon!";
+const url = `${env.NEXT_PUBLIC_BASE_URL}/categories`;
+const imageUrl = `${env.NEXT_PUBLIC_OPENGRAPH_PIC}`;
+const keywords = [
+  "AI for healthcare",
+  "medical imaging analysis",
+  "electronic health records",
+  "drug discovery",
+  "AI in healthcare",
+  "AI in medicine",
+];
+
+export const metadata: Metadata = generateCommonMetadata({
+  title,
+  description,
+  url,
+  imageUrl,
+  keywords,
+});
 
 export default async function CategoriesPage() {
   const categories = await getCategories();
--- src/app/companies/page.tsx
+++ src/app/companies/page.tsx
@@ -1,6 +1,31 @@
+import { Metadata } from "next";
+
 import { CompaniesList } from "@/components/companies-list";
 import SectionHeader from "@/components/section-header";
+import { env } from "@/env/client";
 import { getCompaniesOverview } from "@/lib/actions";
+import { generateCommonMetadata } from "@/lib/metadata";
+
+const title = "All Companies of AI for Healthcare Hub";
+const description = "Explore all companies listed on AI for Healthcare Hub";
+const url = `${env.NEXT_PUBLIC_BASE_URL}/companies`;
+const imageUrl = `${env.NEXT_PUBLIC_OPENGRAPH_PIC}`;
+const keywords = [
+  "AI for healthcare",
+  "medical imaging analysis",
+  "electronic health records",
+  "drug discovery",
+  "AI in healthcare",
+  "AI in medicine",
+];
+
+export const metadata: Metadata = generateCommonMetadata({
+  title,
+  description,
+  url,
+  imageUrl,
+  keywords,
+});
 
 export default async function CompaniesPage() {
   const companies = await getCompaniesOverview();
