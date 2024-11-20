28b5d5f6513486150c7fc6b68b66ccf93f5cce07
Arnaud <arnaud.obri@proton.me>
2024-11-20 15:12:26 +0100

    Update page.tsx

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,9 @@
+import SectionHeader from "@/components/section-header";
+
+export default function BlogPage() {
+  return (
+    <section className="container mx-auto min-h-screen py-20">
+      <SectionHeader title="Blog" />
+    </section>
+  );
+}
