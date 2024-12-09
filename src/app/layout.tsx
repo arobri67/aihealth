ba7c54238156ee3e5687bc81f3f7e40551f583c4
Arnaud <arnaud.obri@proton.me>
2024-12-09 15:31:08 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -86,7 +86,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="h-full">
+    <html lang="en" className="h-full" suppressHydrationWarning>
       <head>
         <link
           rel="icon"
